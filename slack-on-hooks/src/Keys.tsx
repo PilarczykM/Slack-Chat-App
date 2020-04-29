6ebe59c5877b29f6a727c488b53c5a81833edb9d
Marcin Pilarczyk <marcinpilarczyk@me.com>
2020-04-29 21:03:43 +0200

    little fix

--- slack-on-hooks/src/Keys.tsx
+++
@@ -1,8 +0,0 @@
-const Keys = {
-  apiKey: "AIzaSyBrufdJW-AGTbz-25NxKO5LBJLWHQcP7Ck",
-  projectId: "slack-on-hooks",
-  senderId: "809514261655",
-  appId: "1:809514261655:web:c1b1d6dabba25c80ac9dd2",
-};
-
-export default Keys;
